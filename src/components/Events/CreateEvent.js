b8fcc0c247564bdb7f1365210c3d7e1bf6371572
frideska <frideskarz28@gmail.com>
2017-10-03 14:22:34 +0200

    Made create event class

---
+++ src/components/Events/CreateEvent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Input, Button } from 'semantic-ui-react'
+
+export default class CreateEvent extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
