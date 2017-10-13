aff75d276a8960d94aa0a650188a58c1d032ff51
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-13 12:05:06 +0200

    what even is testing???

---
+++ src/tests/Notes/Notes-test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Notes from '../../components/Notes/Notes';
+import renderer from 'react-test-renderer';
+
+test('Link changes the class when hovered', () => {
+    const component = renderer.create(
+        <Notes> </Notes>
+    );
+    
+
+});
+
