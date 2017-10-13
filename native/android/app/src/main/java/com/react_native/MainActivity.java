dd55a3360ff7d3bb326fa446db973fba7d65ea6a
tsm121 <tsm121@gmail.com>
2017-10-13 14:31:30 +0200

    created new react native project. Old project was corrupted

--- native/android/app/src/main/java/com/react_native/MainActivity.java
+++ native/android/app/src/main/java/com/react_native/MainActivity.java
@@ -1,4 +1,4 @@
-package com.native;
+package com.react_native;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "native";
+        return "react_native";
     }
 }
