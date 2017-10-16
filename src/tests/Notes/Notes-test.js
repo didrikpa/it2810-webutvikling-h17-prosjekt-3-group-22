867c353083e08b00341f596507b7aa37c4c3c6fa
SkylineTardis <martiagr@stud.ntnu.no>
2017-10-16 11:09:03 +0200

    installed enzyme and added few test to note

--- src/tests/Notes/Notes-test.js
+++ src/tests/Notes/Notes-test.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import Notes from '../../components/Notes/Notes';
-import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 
-test('dunno yet', () => {
-    const component = renderer.create(
-        <Notes> </Notes>
-    );
+describe("Note", () => {
+    let props;
+    let mountedNote;
+    const note = () => {
+        if (!mountedNote) {
+            mountedNote = mount(
+                <Notes {...props} />
+            );
+        }
+        return mountedNote;
+    }
 
+    beforeEach(() => {
+        props = {
+            note: undefined,
+            onButtonSaveClick: undefined,
 
+        };
+        mountedNote = undefined;
+    });
+
+    it("always renders a div", () => {
+        const divs = note().find("div");
+        expect(divs.length).toBeGreaterThan(0);
+    });
+
+    describe("the rendered div", () => {
+        it("contains everything else that gets rendered", () => {
+            const divs = lockScreen().find("div");
+            // When using .find, enzyme arranges the nodes in order such
+            // that the outermost node is first in the list. So we can
+            // use .first() to get the outermost div.
+            const wrappingDiv = divs.first();
+
+            // Enzyme omits the outermost node when using the .children()
+            // method on note(). This is annoying, but we can use it
+            // to verify that wrappingDiv contains everything else this
+            // component renders.
+            expect(wrappingDiv.children()).toEqual(note().children());
+        });
+    });
 });
-
