2445f11505c25a211c0a1b69f4172d150362257a
didrikpa <didrikpa@gmail.com>
2017-10-16 14:18:33 +0200

    added enzyme

---
+++ src/tests/Todo/EditTodoModal.react-test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import EditTodoModal from '../../components/Todo/.EditTodoModal'
+import {shallow} from 'enzyme'
+
+
+test('Test something in this module... ', () => {
+  const component = renderer.create(
+    <EditTodoModal></EditTodoModal>
+  );
+
+});
--- src/tests/Todo/EditTodoModal.test.js
+++
@@ -1,2 +0,0 @@
-const editTodoModal = require('../../components/Todo/.EditTodoModal');
-
