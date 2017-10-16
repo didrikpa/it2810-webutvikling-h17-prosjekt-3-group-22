86b227897d53fa9d114e70cc467b02c45c341006
Unknown <pciverse@stud.ntnu.no>
2017-10-16 23:52:47 +0200

    asdf

--- native/App.js
+++ native/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import { Container } from 'native-base';
-import {
-  Platform,
-  StyleSheet,
-} from 'react-native';
-import Navbar from './Navbar'
 import TodoContainer from './Todo/TodoContainer'
 
 export default class App extends Component<{}> {
