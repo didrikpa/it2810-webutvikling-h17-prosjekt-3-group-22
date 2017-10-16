9f1088f93c57b4860913155fff6b2888c71d4408
langokalla <martin.langmo.karlstrom@gmail.com>
2017-10-16 21:23:12 +0200

    Moved NavFooter to bottom, router problems.

--- native/App.js
+++ native/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { Container, Header, Content, Tab, Tabs, ScrollView, Footer } from 'native-base'
-import { Platform, StyleSheet, View } from 'react-native'
-import { Router } from 'react-native-router-flux'
 
-import NavFooter from './components/NavFooter'
 import Routes from './Routes'
 
 export default class App extends Component {
