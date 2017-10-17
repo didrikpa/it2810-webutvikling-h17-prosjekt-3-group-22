6d03e893e7011ae80100167cbaec77837c6b64a7
didrikpa <didrikpa@gmail.com>
2017-10-17 10:35:55 +0200

    Added test for EditTodoModal and a snapshots-folder

---
+++ src/tests/Todo/EditTodoModal.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Todo from '../../components/Todo/EditTodoModal';
+import Adapter from 'enzyme-adapter-react-15'
+import toJson from 'enzyme-to-json'
+import moment from 'moment'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('Todo component should render as expected', () => {
+  const component = shallow(<EditToDoModal/>);
+  const tree = toJson(component);
+  console.log(tree);
+  expect(tree).toMatchSnapshot();
+});
