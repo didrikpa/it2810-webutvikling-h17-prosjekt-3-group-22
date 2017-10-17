fe0e66ddb6eaf956315cc4a6df01c9ab86993be2
didrikpa <didrikpa@gmail.com>
2017-10-17 14:02:29 +0200

    Added rendering tests for different Todo modules

--- src/tests/Todo/EditTodoModal.test.js
+++ src/tests/Todo/EditTodoModal.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Todo from '../../components/Todo/EditTodoModal';
+import EditTodoModal from '../../components/Todo/EditTodoModal';
 import Adapter from 'enzyme-adapter-react-15'
 import toJson from 'enzyme-to-json'
+import moment from 'moment'
+
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('Todo component should render as expected', () => {
-  const component = shallow(<EditToDoModal/>);
+test('Todo component should render as expected when isOpen is true', () => {
+  const tempContent = "Some text to check3";
+  const content = "Some text to check2";
+
+  const component = shallow(<EditTodoModal content={content} tempContent={tempContent} date={moment()} isOpen={true}/>);
+  const tree = toJson(component);
+  console.log(tree);
+  expect(tree).toMatchSnapshot();
+});
+
+test('Todo component should not render at all when isOpen is false', () => {
+  const component = shallow(<EditTodoModal isOpen={false}/>);
   const tree = toJson(component);
   console.log(tree);
   expect(tree).toMatchSnapshot();
