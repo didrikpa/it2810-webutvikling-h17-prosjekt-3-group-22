57ac333643c296e67dc90cba0dab4239368fac7a
oleast <oleast@stud.ntnu.no>
2017-10-18 15:17:55 +0200

    Adding comments to DeleteModal

--- web/src/components/DeleteModal.js
+++ web/src/components/DeleteModal.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react'
 import { Button, Modal } from 'semantic-ui-react'
 
+/**
+ * DeleteModal is used as a popup to ask if you are sure you want to delete something.
+ * It is located outside the sub-apps because it is reusable across all of them, and for any potential new additions.
+ */
+
 class DeleteModal extends Component {
     constructor (props) {
         super(props)
 
+        /**
+         * Set initial state of the Component to be closed when first created.
+         */
         this.state = {
             open: false
         }
     }
 
+    /**
+     * This method is used to open the modal.
+     */
     handleOpen = () => {
-        this.setState({
-            open: true
-        }, () => {
-            console.log('open')
-        })
+        this.setState({ open: true })
     }
 
+    /**
+     * This method is used to close the Modal.
+     */
     handleClose = () => {
-        this.setState({
-            open: false
-        }, () => {
-            console.log('closed')
-        })
+        this.setState({ open: false })
     }
 
     render () {
+        /**
+         * Fetch the 'open' variable from Component state.
+         * Fetch the 'handleDelete' method and 'title' variable passed from parent.
+         */
         const { open } = this.state
         const { handleDelete, title } = this.props
         return (
